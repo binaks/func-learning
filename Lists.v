0da598a91d6298a8cb6dce2aa67c16a3cfa5a00c
Bianca <bianca.rc@hotmail.com>
2017-03-20 03:21:48 -0300

    Add files via upload

---
+++ Lists.v
@@ -0,0 +1,67 @@
+Require Export Induction.
+Module NatList.
+
+Inductive natprod : Type :=
+| pair : nat -> nat -> natprod.
+
+Check (pair 3 5).
+
+Definition fst (p : natprod) : nat :=
+  match p with
+  | pair x y => x
+  end.
+
+Definition snd (p : natprod) : nat :=
+  match p with
+  | pair x y => y
+  end.
+
+Compute (fst (pair 3 5)).
+
+Notation "( x , y )" := (pair x y).
+
+Compute (fst (3,5)).
+
+Definition fst' (p : natprod) : nat :=
+  match p with
+  | (x,y) => x
+  end.
+
+Definition snd' (p : natprod) : nat :=
+  match p with
+  | (x,y) => y
+  end.
+
+Definition swap_pair (p : natprod) : natprod :=
+  match p with
+  | (x,y) => (y,x)
+  end.
+
+Theorem surjective_pairing' : forall (n m : nat),
+  (n,m) = (fst (n,m), snd (n,m)).
+Proof.
+  reflexivity.
+Qed.
+
+Theorem surjective_pairing : forall (p : natprod),
+  p = (fst p, snd p).
+Proof.
+  intros p. destruct p as [n m].
+  simpl. reflexivity.
+Qed.
+
+Theorem snd_fst_is_swap : forall (p : natprod),
+  fst (swap_pair p) = snd p.
+Proof.
+  intros p. destruct p as [n m].
+  simpl. reflexivity.
+Qed.
+
+Theorem fst_swap_is_snd : forall (p : natprod),
+  fst (swap_pair p) = snd p.
+Proof.
+  intros p. destruct p as [n m].
+  simpl. reflexivity.
+Qed.
+
+
