1a30230e812a882fe4ae7aef97ee35ce01841178
Bianca <bianca.rc@hotmail.com>
2017-03-20 02:59:57 -0300

    Add files via upload

--- Induction.v
+++ Induction.v
@@ -107,9 +107,128 @@ Proof.
     rewrite -> plus_assoc'. reflexivity.
 Qed.
 
+Theorem mult_n_Sm : forall n m : nat,
+  n * S m = n * m + n.
+Proof.
+  intros n m. induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    rewrite -> plus_n_Sm.
+    rewrite -> plus_n_Sm.
+    rewrite <- plus_assoc'.
+    reflexivity.
+Qed.
+
 Theorem mult_comm : forall m n : nat,
   m * n = n * m.
 Proof.
   intros m n. induction n as [|n' IHn'].
   - simpl. rewrite -> mult_0_r. reflexivity.
-  - 
+  - destruct m.
+    + simpl. rewrite -> mult_0_r. reflexivity.
+    + simpl. rewrite <- IHn'. simpl.
+      rewrite -> plus_swap.
+      rewrite -> mult_n_Sm.
+      assert (H: m * n' + m = m + m * n').
+      { rewrite -> plus_comm. reflexivity. }
+      rewrite -> H. reflexivity.
+Qed.
+
+Theorem leb_refl : forall n : nat,
+  true = leb n n.
+Proof.
+  intros n. induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite <- IHn'. reflexivity.
+Qed.
+
+Theorem zero_nbeq_S : forall n : nat,
+  beq_nat 0 (S n) = false.
+Proof.
+  intros n. simpl. reflexivity.
+Qed.
+
+Theorem andb_false_r : forall b : bool,
+  andb b false = false.
+Proof.
+  intros b. destruct b.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Theorem plus_ble_compat_l : forall n m p : nat,
+  leb n m = true -> leb (p + n) (p + m) = true.
+Proof.
+  intros n m p H.
+  induction p as [|p' IHn'].
+  - simpl. rewrite -> H. reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem S_nbeq_0 : forall n : nat,
+  beq_nat (S n) 0 = false.
+Proof.
+  intros n. simpl. reflexivity.
+Qed.
+
+Theorem mult_1_l : forall n : nat, 1 * n = n.
+Proof.
+  intros n. simpl. rewrite <- plus_n_0.
+  reflexivity.
+Qed.
+
+Theorem all3_spec : forall b c : bool,
+  orb
+    (andb b c)
+      (orb (negb b)
+        (negb c))
+  = true.
+Proof.
+  intros b c. destruct b.
+  - simpl. destruct c.
+    + simpl. reflexivity.
+    + simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Theorem mult_plus_distr_r : forall n m p : nat,
+  (n + m) * p = (n * p) + (m * p).
+Proof.
+  intros n m p. induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    rewrite -> plus_assoc'.
+    reflexivity.
+Qed.
+
+Theorem mult_assoc : forall n m p : nat,
+  n * (m * p) = (n * m) * p.
+Proof.
+  intros n m p.
+  induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    rewrite -> mult_plus_distr_r.
+    reflexivity.
+Qed.
+
+Theorem beq_nat_refl : forall n : nat,
+  true = beq_nat n n.
+Proof.
+  intros n. induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite <- IHn'. reflexivity.
+Qed.
+
+Theorem plus_swap' : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  intros n m p. destruct n.
+  - simpl. reflexivity.
+  - simpl. rewrite <- plus_n_Sm.
+    rewrite -> plus_assoc'.
+    rewrite -> plus_assoc'.
+    replace (n + m) with (m + n).
+    + reflexivity.
+    + rewrite -> plus_comm. reflexivity.
+Qed.
